b263156332dbe188adfdbb2d0ae6a8855e2f2ea9
pipaek <uhmppi1@gmail.com>
2022-11-15 17:07:34 +0900

    install fixed

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 conda install conda
-conda install conda_build
+conda install conda-build
 conda install tqdm
 conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
 conda install -c huggingface transformers==4.14.1 tokenizers==0.10.3
